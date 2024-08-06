00f069d389cf06f5ba7a85d81003c9e93e60f8db
msvalandro <msvalandro@proton.me>
2024-08-06 20:27:35 +0100

    feat: add animation to quiz cards on home screen

--- src/components/QuizCard/index.tsx
+++ src/components/QuizCard/index.tsx
@@ -4,21 +4,30 @@ import {
   TouchableOpacityProps,
   View,
 } from 'react-native'
+import Animated, { FadeInUp } from 'react-native-reanimated'
 
 import { QUIZZES } from '../../data/quizzes'
 import { THEME } from '../../styles/theme'
 import { LevelBars } from '../LevelBars'
 import { styles } from './styles'
 
+const AnimatedTouchableOpacity =
+  Animated.createAnimatedComponent(TouchableOpacity)
+
 type Props = TouchableOpacityProps & {
+  index: number
   data: (typeof QUIZZES)[0]
 }
 
-export function QuizCard({ data, ...rest }: Props) {
+export function QuizCard({ index, data, ...rest }: Props) {
   const Icon = data.svg
 
   return (
-    <TouchableOpacity style={styles.container} {...rest}>
+    <AnimatedTouchableOpacity
+      entering={FadeInUp.delay(index * 100)}
+      style={styles.container}
+      {...rest}
+    >
       <View style={styles.header}>
         <View style={styles.iconContainer}>
           {Icon && <Icon size={24} color={THEME.COLORS.GREY_100} />}
@@ -28,6 +37,6 @@ export function QuizCard({ data, ...rest }: Props) {
       </View>
 
       <Text style={styles.title}>{data.title}</Text>
-    </TouchableOpacity>
+    </AnimatedTouchableOpacity>
   )
 }
