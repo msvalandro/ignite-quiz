68540aa1e7e19d0a7962404cd6c7d38076ca6948
msvalandro <msvalandro@proton.me>
2024-08-07 17:12:34 +0100

    feat: call JS function to skip question on animation

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -6,6 +6,7 @@ import Animated, {
   Easing,
   Extrapolate,
   interpolate,
+  runOnJS,
   useAnimatedScrollHandler,
   useAnimatedStyle,
   useSharedValue,
@@ -31,6 +32,7 @@ interface Params {
 type QuizProps = (typeof QUIZ)[0]
 
 const CARD_INCLINATION = 10
+const CARD_SKIP_AREA = -200
 
 export function Quiz() {
   const [points, setPoints] = useState(0)
@@ -167,7 +169,11 @@ export function Quiz() {
         cardPosition.value = event.translationX
       }
     })
-    .onEnd(() => {
+    .onEnd((event) => {
+      if (event.translationX < CARD_SKIP_AREA) {
+        runOnJS(handleSkipConfirm)()
+      }
+
       cardPosition.value = withTiming(0)
     })
 
