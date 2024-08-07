8299cebc669917017be55613927eb5fa83462405
msvalandro <msvalandro@proton.me>
2024-08-07 19:15:03 +0100

    feat: play sound and vibrate device to give feedback on questions

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -1,4 +1,6 @@
 import { useNavigation, useRoute } from '@react-navigation/native'
+import { Audio } from 'expo-av'
+import * as Haptics from 'expo-haptics'
 import { useEffect, useState } from 'react'
 import { Alert, Text, View } from 'react-native'
 import { Gesture, GestureDetector } from 'react-native-gesture-handler'
@@ -55,6 +57,17 @@ export function Quiz() {
   const route = useRoute()
   const { id } = route.params as Params
 
+  async function playSound(isCorrect: boolean) {
+    const file = isCorrect
+      ? require('../../assets/correct.mp3')
+      : require('../../assets/wrong.mp3')
+
+    const { sound } = await Audio.Sound.createAsync(file, { shouldPlay: true })
+
+    await sound.setPositionAsync(0)
+    await sound.playAsync()
+  }
+
   function handleSkipConfirm() {
     Alert.alert('Pular', 'Deseja realmente pular a questão?', [
       { text: 'Sim', onPress: () => handleNextQuestion() },
@@ -91,9 +104,11 @@ export function Quiz() {
     }
 
     if (quiz.questions[currentQuestion].correct === alternativeSelected) {
+      await playSound(true)
       setStatusReply(1)
       setPoints((prevState) => prevState + 1)
     } else {
+      await playSound(false)
       setStatusReply(2)
       shakeAnimation()
     }
@@ -117,7 +132,9 @@ export function Quiz() {
     return true
   }
 
-  function shakeAnimation() {
+  async function shakeAnimation() {
+    await Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error)
+
     shake.value = withSequence(
       withTiming(3, { duration: 400, easing: Easing.bounce }),
       withTiming(0, undefined, (finished) => {
