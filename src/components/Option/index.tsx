9d3989c04c7d34056bc2f3eb5a8ecb6eae233e95
msvalandro <msvalandro@proton.me>
2024-08-07 18:28:52 +0100

    feat: add check icon with skia

--- src/components/Option/index.tsx
+++ src/components/Option/index.tsx
@@ -1,6 +1,12 @@
+import { Canvas, Path, Skia } from '@shopify/react-native-skia'
 import { Text, TouchableOpacity, TouchableOpacityProps } from 'react-native'
 
+import { THEME } from '../../styles/theme'
 import { styles } from './styles'
+
+const CHECK_SIZE = 28
+const CHECK_STROKE = 2
+const RADIUS = (CHECK_SIZE - CHECK_STROKE) / 2
 
 type Props = TouchableOpacityProps & {
   checked: boolean
@@ -8,12 +14,33 @@ type Props = TouchableOpacityProps & {
 }
 
 export function Option({ checked, title, ...rest }: Props) {
+  const path = Skia.Path.Make()
+  path.addCircle(CHECK_SIZE, CHECK_SIZE, RADIUS)
+
   return (
     <TouchableOpacity
       style={[styles.container, checked && styles.checked]}
       {...rest}
     >
       <Text style={styles.title}>{title}</Text>
+
+      <Canvas style={{ height: CHECK_SIZE * 2, width: CHECK_SIZE * 2 }}>
+        <Path
+          path={path}
+          color={THEME.COLORS.GREY_500}
+          style="stroke"
+          strokeWidth={CHECK_STROKE}
+        />
+
+        <Path
+          path={path}
+          color={THEME.COLORS.BRAND_LIGHT}
+          style="stroke"
+          strokeWidth={CHECK_STROKE}
+          start={0}
+          end={0.5}
+        />
+      </Canvas>
     </TouchableOpacity>
   )
 }
